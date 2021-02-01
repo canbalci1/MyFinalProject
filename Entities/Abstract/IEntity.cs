b3a5683d87e8fc48b6a64130d03f837e23ebefad
Can Balcı <canbalci@outlook.com.tr>
2021-02-01 16:49:52 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity iplement eden class bir veretabanı tablosudur.
+    public interface IEntity
+    {
+
+    }
+}
