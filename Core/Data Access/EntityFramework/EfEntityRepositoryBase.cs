61dbf28b209654fb225547f3163ebb317580f039
Can Balcı <canbalci@outlook.com.tr>
2021-02-08 14:51:58 +0300

    08,02,2021

---
+++ Core/Data Access/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Data_Access.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>
+    {
+
+
+    }
+}
