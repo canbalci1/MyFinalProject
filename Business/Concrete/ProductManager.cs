b3a2e2263898306fffac8b9f8e328a521fd53cbb
Can Balcı <canbalci@outlook.com.tr>
2021-03-16 01:53:04 +0300

    16.03.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
+using Business.CCS;
 using Business.Constans;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -11,31 +13,49 @@ using Entities.DTOs;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
-        IProductDal _productDal;
 
-        public ProductManager(IProductDal productDal)
+        IProductDal _productDal;
+        ICategoryService _categoryService;
+        
+
+        public ProductManager(IProductDal productDal , ICategoryService categoryService )
         {
             _productDal = productDal;
+            _categoryService = categoryService;
         }
 
         [ValidationAspect(typeof(ProductValidator))]
 
         public IResult Add(Product product)
         {
-            
+            //Bir kategoride en fazla 10 ürün olabilir.
+            //Aynı isimde ürün eklenemez.
+            //Eğer mevcut kategori sayısı 15 i geçtiyse sisteme yeni ürün eklenemez
+           IResult result= BusinessRule.Run(CheckIfProductNameExists(product.ProductName),CheckIfProductCountOfCategoryCorrect(product.CategoryId),
+               CheckIfCategoryLimitExceded());
+            if (result!=null)
+            {
+                return result;
+            }
 
-            ValidationTool.Validate(new ProductValidator(), product);
-           
-            //business codes
-            
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAddded);
+
+
+           
+
+            
+
+            //ValidationTool.Validate(new ProductValidator(), product);
+            //business codes
+
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -67,6 +87,44 @@ namespace Business.Concrete
         public IDataResult< List<ProductDetailDto>> GetProductDetails()
         {
             return new SuccessDataResult<List<ProductDetailDto>>( _productDal.GetProductDetails());
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountofCategoryError);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count>15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+            return new SuccessResult();
+
+
         }
     }
 }
--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constans
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime="Sistem Bakımda";
         public static string ProductsListed= "Ürünler listelendi";
+        public static string ProductCountofCategoryError= "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded="Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
